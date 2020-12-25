8b8a419405871875dbfbd12793d33f0a114026ae
Jye Karl-Perry <me@jye.guru>
2020-12-25 15:11:44 +1100

    Add option to force-kill all arbiter processes

--- Source/Scripts/Run.cmd
+++ Source/Scripts/Run.cmd
@@ -23,22 +23,35 @@ if ($hosting) {
     Write-Host "Done! Game loaded in Host Mode." -ForegroundColor Green
 
     # Script block required due to mod bug: https://github.com/rwmt/Multiplayer/issues/124
-    Write-Host "Watching for bugged arbiter processes ..." -ForegroundColor Blue
     while ($true) {
-        Get-WmiObject Win32_Process | Where-Object { $_.Name -eq "RimWorldWin64.exe" } | Foreach-Object {
-            if (($_.CommandLine -like "*-arbiter*") -and ($_.CommandLine -like "*-savedatafolder=""*")) {
-                $arbiterpid = $_.ProcessId
-                $commandline = $_.CommandLine -Replace '^.*RimWorldWin64.exe\" ' -Replace '-savedatafolder.*$', """-savedatafolder=$(Resolve-Path $configpath)"""
-                Write-Host "Arbiter found: PID $arbiterpid" -ForegroundColor Green
-                Write-Host "Original Command: $($_.CommandLine)"
-                Write-Host "Fixed Command: $commandline"
-                Write-Host "- Stopping ..."
-                Stop-Process -Id $arbiterpid -Force
-                Write-Host "- Restarting ..."
-                Start-Process -FilePath ".\RimWorldWin64.exe" -ArgumentList $commandline
-            } 
+        Write-Host "Watching for bugged arbiter processes [Press Q to kill all] ..." -ForegroundColor Blue
+        while (!([Console]::KeyAvailable)) {
+            Get-WmiObject Win32_Process | Where-Object { $_.Name -eq "RimWorldWin64.exe" } | Foreach-Object {
+                if (($_.CommandLine -like "*-arbiter*") -and ($_.CommandLine -like "*-savedatafolder=""*")) {
+                    $arbiterpid = $_.ProcessId
+                    $commandline = $_.CommandLine -Replace '^.*RimWorldWin64.exe\" ' -Replace '-savedatafolder.*$', """-savedatafolder=$(Resolve-Path $configpath)"""
+                    Write-Host "Arbiter found: PID $arbiterpid" -ForegroundColor Green
+                    Write-Host "Original Command: $($_.CommandLine)"
+                    Write-Host "Fixed Command: $commandline"
+                    Write-Host "- Stopping ..."
+                    Stop-Process -Id $arbiterpid -Force
+                    Write-Host "- Restarting ..."
+                    Start-Process -FilePath ".\RimWorldWin64.exe" -ArgumentList $commandline
+                } 
+            }
+            Start-Sleep -Seconds 1      
         }
-        Start-Sleep -Seconds 1      
+        $pressed = [Console]::ReadKey($true)
+        if ($pressed.Key -eq "Q") {
+            Write-Host "- Force-Kill Triggered ..."
+            Get-WmiObject Win32_Process | Where-Object { $_.Name -eq "RimWorldWin64.exe" } | Foreach-Object {
+                if ($_.CommandLine -like "*-arbiter*") {
+                    $id = $_.ProcessId
+                    Stop-Process -Id $id -Force
+                    Write-Host "Force killed PID $id!" -ForegroundColor Green
+                }
+            }
+        }
     } # End workaround
 }
 else {
